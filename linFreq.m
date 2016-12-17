30e8bb199a1ecac6f5f11b59a75bfd1e81fd7203
nicobe831 <nico-berck@hotmail.com>
2016-12-17 17:33:44 +0100

    compute linear natural frequencies of the system

---
+++ linFreq.m
@@ -0,0 +1,8 @@
+%This script computes the linear natural frequencies of the 2 dof system
+
+load('initialMatrix.mat')
+[~,d] = eig(K,M);
+f = sqrt(diag(d)); %natural frequiencies in rad/s
+f = f/2/pi; %natural frequencies in Hz
+
+clear d;
