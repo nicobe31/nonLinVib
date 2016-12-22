8367d44c9d531706b34ad6ee3601e6f3c307625b
nicobe831 <nico-berck@hotmail.com>
2016-12-22 16:22:59 +0100

    transform script force into a function

--- forces.m
+++ forces.m
@@ -1,63 +1,95 @@
-%% linear sine sweep
-ampl = 1; %N
-f0 = 5; %Hz
-f1 = 40; %Hz
-r = 2; %rate Hz/s
-phi0 = 0; %degree
-samplingFreq = 100; %Hz
+function [F,t] = forces(type,param1,param2, param3, param4, param5)
+%%
+% [F,t] = forces(type,param1,param2)
+% 
+% This function determine the force et the corresponding time vector.
+% 
+% Arguments: type: type of force: 'sineSweep' or 'multisine'
+% if type= sineSweep: param1 = amplitude [N] 
+%                     param2 = starting frequency [Hz]
+%                     param3 = ending frequency [Hz]
+%                     param4 = sweep rate [Hz/s]
+%                     param5: not used
+% if type= multisine: param1 = amplitude [N] 
+%                     param2 = starting frequency [Hz]
+%                     param3 = ending frequency [Hz]
+%                     param4 = number of point for the time vector
+%                     param5 = number of frequencies used to represent the
+%                     door function
+%%
 
-phi0 = phi0*pi/180; % °-> rad
-t0 = 0;
-t1 = (f1-f0)/r;
-t = t0:1/samplingFreq:t1;
+switch type
+    case 'sineSweep'
+    %% linear sine sweep
+    ampl = param1; %N
+    f0 = param2; %Hz
+    f1 = param3; %Hz
+    r = param4; %rate Hz/s
+    
+        % parameters
+        phi0 = 0; %initial phase angle degree
+        samplingFreq = 100; %Hz
 
-arg1 = 2*pi*f0*(t-t0);
-arg2 = 2*pi*r*(t-t0).^2/2;
-arg = arg1 + arg2 + phi0;
+    phi0 = phi0*pi/180; % °-> rad
+    t0 = 0;
+    t1 = (f1-f0)/r;
+    seriet = t0:1/samplingFreq:t1;
 
-u = ampl*sin(arg);
+    arg1 = 2*pi*f0*(seriet-t0);
+    arg2 = 2*pi*r*(seriet-t0).^2/2;
+    arg = arg1 + arg2 + phi0;
 
+    F = ampl*sin(arg);
 
-%% random
-Nt = 2^13;
-Neff = 1e2;
-f0 = 5;
-f1 = 40;
-t0 = 0;
-t1 = 20;
+    case 'multisine' 
+    %% multisine with random phase
+    ampl = param1; 
+    f0 = param2;
+    f1 = param3;
+    Nt = param4;
+    Neff = param5;
+    
+        % parameters 
+        t0 = 0;
+        t1 = 20;
+        repetition = 4; % power of 2
+       
+    serieNt = Nt/repetition;
+    mod(serieNt,1)
+    seriet = linspace(t0,t1/repetition,serieNt);
+    dt = seriet(2)-seriet(1);
+    samplingFreq = 1/dt; %Hz
 
-t = linspace(t0,t1,Nt);
-samplingFreq = 1/(t(2)-t(1)); %Hz
+    N = Neff*samplingFreq/(f1-f0);
+    k0 = floor(f0*N/samplingFreq);
+    k1 = ceil(f1*N/samplingFreq);
 
-N = Neff*samplingFreq/(f1-f0);
-k0 = floor(f0*N/samplingFreq);
-k1 = ceil(f1*N/samplingFreq);
-% df = (f1-f0)/Neff;
-% N1 = floor(f0/df);
-% N2 = floor(f1/df);
+    phik = (2*pi*rand(k1-k0+1,1))-pi;
 
-phik = (2*pi*rand(k1-k0+1,1))-pi;
-% Uk = zeros(N1,1);
-% Uk(1:N2) = ones(N2,1);
+    arg1 = 1i*2*pi*[k0:k1]'*samplingFreq/N*seriet;
+    arg2 = 1i*phik;
+    arg = bsxfun(@plus, arg1, arg2);
 
-arg1 = 1i*2*pi*[k0:k1]'*samplingFreq/N*t;
-% arg2 = 1i*diag(phik)*ones(size(arg1));
-arg2 = 1i*phik;
-% arg = arg1 + arg2;
-arg = bsxfun(@plus, arg1, arg2);
-% factor1 = exp(arg1);
-% factor2 = Uk.*exp(arg2);
+    sines = exp(arg);
 
-% sines = bsxfun(@times,factor1, factor2);
-sines = exp(arg);
+    u  = real(N^(-1/2)*ampl*sum(sines,1));
+    
+    F = zeros(1,Nt);
+    t = zeros(1,Nt);
+    F(1:serieNt) = u;
+    t(1:serieNt) = seriet;
+    for rep = 2:repetition
+        F((rep-1)*serieNt + 1: rep*serieNt) = u;
+        t((rep-1)*serieNt + 1: rep*serieNt) = seriet + t((rep-1)*serieNt) + dt;
+    end
+% 
+% y = fft(u); 
+% fd = abs(y/Nt);
+% p2 = fd(1:Nt/2+1);
+% p2(2:end-1) = 2*p2(2:end-1);
+% f = samplingFreq*(0:(Nt/2))/Nt;
+% plot(f,p2)
 
-u  = real(N^(-1/2)*sum(sines,1));
-
-y = fft(u); 
-fd = abs(y/Nt);
-p2 = fd(1:Nt/2+1);
-p2(2:end-1) = 2*p2(2:end-1);
-f = samplingFreq*(0:(Nt/2))/Nt;
-plot(f,p2)
+end
 
 
